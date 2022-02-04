9b5f9554a58d9d2e3bd66554a95250aa85a4adc0
Jovit Mathew <70875875+Jovit-Mathew236@users.noreply.github.com>
2022-02-05 00:09:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function menuFunc() {
     var x = document.getElementById("menu");
-  if (x.style.display === "none") {
-    x.style.display = "flex";
+  if (x.style.top === "-160px") {
+    // x.style.display = "flex";
+    x.style.top = "0px"
   } else {
-    x.style.display = "none";
+    // x.style.display = "none";
+    x.style.top = "-160px"
   }
   }
