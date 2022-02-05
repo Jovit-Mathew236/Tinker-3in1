acefbf75f211871bc7cf1ff5bb15b09d9ddb4599
Jovit Mathew <70875875+Jovit-Mathew236@users.noreply.github.com>
2022-02-06 01:06:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function menuFunc() {
     var x = document.getElementById("menu");
-  if (x.style.top === "-160px") {
+  if (x.style.top === "0px") {
     // x.style.display = "flex";
-    x.style.top = "0px"
+    x.style.top = "-160px"
   } else {
     // x.style.display = "none";
-    x.style.top = "-160px"
+    x.style.top = "0px"
   }
   }
